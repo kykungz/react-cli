c034651ca4469fce7df5b3fde4ef1ebba03508ff
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-03-29 01:03:54 +0700

    :white_check_mark: Add tests for parser toString

--- __tests__/parser.spec.js
+++ __tests__/parser.spec.js
@@ -3,7 +3,7 @@
 const parser = require('../src/parser')
 
 describe('parser', () => {
-  describe('.concat ', () => {
+  describe('concat', () => {
     test('should return merged template', () => {
       const base = {
         import: 'import a',
@@ -21,6 +21,30 @@ describe('parser', () => {
         export: ''
       }
       expect(parser.concat(base, template)).toEqual(expected)
+    })
+  })
+
+  describe('toString ', () => {
+    test('should return string according to template', () => {
+      const template = {
+        import: 'import a\nimport b',
+        prefix: 'prefix a',
+        body: '',
+        suffix: 'suffix a\b suffix b',
+        export: 'export a'
+      }
+      const expected = `
+        import a
+        import b
+
+        prefix a
+
+        suffix a
+        suffix b
+
+        export a
+      `
+      expect(parser.toString(template)).toEqual(expected)
     })
   })
 })
